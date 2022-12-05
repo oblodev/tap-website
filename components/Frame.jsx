4288b99eab55762a71f5c0e255ffa6fbeedafe73
Dawid Kostka <dpkostka@gmail.com>
2022-12-05 15:34:07 +0100

    added iframe + funct.

--- components/Frame.jsx
+++ components/Frame.jsx
@@ -1,23 +1,47 @@
 import styles from "../styles/Frame.module.scss";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+import { FaFacebookSquare } from "react-icons/fa";
 
 function Frame() {
+  const [toggleSide, setToggleSide] = useState(true);
   return (
-    <motion.div
-      className={styles.frame}
-      whileInView={{ x: [-300, 0], opacity: [0, 1] }}
-      transition={{ delay: 3 }}
-    >
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Ffacebook.com%2FDoktorBully%2F&tabs=timeline&width=500&height=580&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width={500}
-        height={580}
-        scrolling="no"
-        frameborder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      ></iframe>
-    </motion.div>
+    <>
+      {toggleSide && (
+        <motion.div
+          className={styles.frame}
+          whileInView={{ x: [-300, 0], opacity: [0, 1] }}
+          transition={{ delay: 0 }}
+        >
+          <div className={styles.x}>
+            <AiOutlineClose onClick={() => setToggleSide(false)} />
+          </div>
+          <div className={styles.framer}>
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Ffacebook.com%2FDoktorBully%2F&tabs=timeline&width=500&height=580&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              width={500}
+              height={580}
+              scrolling="no"
+              frameborder="0"
+              allowfullscreen="true"
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+            ></iframe>
+          </div>
+        </motion.div>
+      )}
+      {!toggleSide && (
+        <motion.div
+          className={styles.frameContainer}
+          whileInView={{ x: [-60, 0], opacity: [0, 1] }}
+          transition={{ delay: 0 }}
+          onClick={() => setToggleSide(true)}
+        >
+          <FaFacebookSquare />
+        </motion.div>
+      )}
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,6 @@ export default function Home() {
       <InfoHero />
       <Frame />
       <Aktuelles />
-
       <Block />
     </div>
   );
