d3cd6da62c2d448bfbec48b862716f36d9835d48
Dawid Kostka <dpkostka@gmail.com>
2023-01-31 12:33:05 +0100

    new one

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
   const data = await fetch(
-    `https://graph.facebook.com/101502198272121/feed?fields=id,message,full_picture,created_time,permalink_url,attachments{media}&limit=6&access_token=${process.env.FACEBOOK_ACCESS_TOKEN}`
+    `https://graph.facebook.com/215912372213533/feed?fields=id,message,full_picture,created_time,permalink_url,attachments{media}&limit=6&access_token=${process.env.FACEBOOK_ACCESS_TOKEN}`
   ).then((response) => response.json());
 
   res.json(data); // Send the response
