bbbfcdc944f7e69e29771607f9c16510cc7741be
Dawid Kostka <dpkostka@gmail.com>
2023-03-24 16:55:03 +0100

    new iris changes

--- components/FullTeam.jsx
+++ components/FullTeam.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/FullTeam.module.scss";
 import team from "../public/images/fullTeam.jpg";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { motion } from "framer-motion";
 
 function FullTeam() {
