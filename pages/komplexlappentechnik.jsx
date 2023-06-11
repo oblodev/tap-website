3e3496dd50a3d6f57884e807acdf7fac4cdcc170
Dawid Kostka <dpkostka@gmail.com>
2023-06-11 21:48:33 +0200

    final changes

--- pages/komplexlappentechnik.jsx
+++ pages/komplexlappentechnik.jsx
@@ -25,9 +25,9 @@ function komplexlappentechnik() {
               werden sämtliche oberen Atemwege beurteilt. Erst nach dieser
               Information kann eine gezielte Therapie stattfinden. Die
               häufigsten Befunde sind Veränderungen am Gaumensegel, den
-              Nasenflügeln und den Stimmtschen. Es können aber auch andere
+              Nasenflügeln und den Stimmtaschen. Es können aber auch andere
               Körperstellen verändert sein. Die Ergebnisse dieser Untersuchung
-              stellen die Grundlage für die Operationsplanung mittels
+              stellen die Grundlage für die Planung der Operation mittels
               Komplexlappentechnik dar.
             </p>
           </div>
@@ -37,11 +37,10 @@ function komplexlappentechnik() {
               Bei den Erkrankungen der oberen Atemwege handelt es sich um ein
               ziemlich komplexes Geschehen. Daher ist es notwendig sämtliche
               Überlegungen, Risiken und Erfolgsaussichten des geplanten
-              Eingriffs eingehend zu besprechen. Dies geschieht an einem
-              separaten Tag von der Operation, außer es handelt sich um einen
-              Notfall, welcher uns diese Zeit nicht mehr gibt. Dadurch ist es
-              möglich alle Aspekte vorab in Ruhe zu besprechen, um dann die
-              richtige Entscheidung für Ihre Knutschkugel zu treffen.
+              Eingriffs eingehend zu besprechen. Dies geschieht meist an einem
+              separaten Tag vor der Operation. Dadurch ist es möglich alle
+              Aspekte vorab in Ruhe zu besprechen, um dann die richtige
+              Entscheidung für Ihre Knutschkugel zu treffen.
             </p>
           </div>
           <div className={styles.hero}>
@@ -50,13 +49,16 @@ function komplexlappentechnik() {
               Die Narkose stellt bei Kurzschnauzern eine besondere
               Herausforderung dar. Unverträglichkeitsreaktionen sind bei diesen
               Rassen besonders häufig und können fatale Auswirkungen haben.
-              Deshalb führen wir eine Reihe an Tests vorab durch, damit das
+              Deshalb führen wir vorab eine Reihe an Tests durch, damit das
               Risiko so gering als möglich gehalten wird. Neben Blutbild,
-              Organprofil, Blutgerinnungstest und Lungenröntgen wird noch die
-              Reaktion auf Narkosemittel durch Monitoring überwacht. Ergänzt
-              wird dieses Monitoring durch ein genau auf diese Situation
-              vorbereitetes und hoch spezialisiertes OP-Team. Für den Fall einer
-              Komplikation muss jeder Handgriff sitzen.
+              Organprofil, Blutelektrolyte, Blutgerinnungstest und Lungenröntgen
+              wird noch die Reaktion auf Narkosemittel durch Monitoring
+              überwacht. Ein vorab durchgeführter Herzultraschall und ein EKG
+              ermöglicht es uns, das Narkoseprotokoll optimal auf den
+              individuellen Patienten abzustimmen. Ergänzt wird die Überwachung
+              durch ein genau auf diese Situation vorbereitetes und hoch
+              spezialisiertes OP-Team. Für den Fall einer Komplikation muss
+              jeder Handgriff sitzen.
             </p>
           </div>
           <div className={styles.hero}>
@@ -65,17 +67,17 @@ function komplexlappentechnik() {
               Hier kommt jetzt die Komplexlappentechnik zum Einsatz! Diese
               Operationstechnik bietet zwei entscheidende Vorteile. Erstens ist
               ein &quot;Nachwachsen&quot; - richtigerweise handelt es sich dabei
-              um Narbenwucherungen - technisch bedingt nicht möglich ist. Diese
-              Komplikation tritt oft bei anderen Opertionstechniken nach Monaten
-              oder Jahren auf. Sie kann so gravierend sein, dass der
-              Gesundheitszustand sogar schlechter als vor dem Eingriff sein
-              kann. Der zweite Vorteil ist, dass Ihre Knutschkugel sofort nach
-              dem Aufwachen wieder zu Ihnen darf. Schlimme Nachblutungen oder
-              Schwellungen sind nicht möglich. Ja, es ist sogar sehr wichtig,
-              dass Ihr Liebling so rasch als möglich wieder zu Ihnen kommt.
-              Kurzschnauzer sind äußerst sensible Hunde und stecken es oft nicht
-              gut weg, wenn sie von ihren Eltern getrennt sind. Im schlimmsten
-              Fall können sie im Zuge eines stationären Aufenthaltes
+              um Narbenwucherungen - technisch bedingt nicht möglich. Diese
+              Komplikation tritt oft bei anderen Operationstechniken nach
+              Monaten oder Jahren auf. Sie kann so gravierend sein, dass der
+              Gesundheitszustand sogar schlechter sein kann als vor dem
+              Eingriff. Der zweite Vorteil ist, dass Ihre Knutschkugel sofort
+              nach dem Aufwachen wieder zu Ihnen darf. Schlimme Nachblutungen
+              oder Schwellungen sind nicht möglich. Ja, es ist sogar sehr
+              wichtig, dass Ihr Liebling so rasch als möglich wieder zu Ihnen
+              kommt. Kurzschnauzer sind äußerst sensible Hunde und stecken es
+              oft nicht gut weg, wenn sie von ihren Eltern getrennt sind. Im
+              schlimmsten Fall können sie im Zuge eines stationären Aufenthaltes
               kollabieren, was dank der Komplexlappentechnik jedoch nicht
               notwendig ist.
             </p>
