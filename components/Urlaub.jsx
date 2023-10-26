17da25ddb3d3bc249ad412cf3f81231e50385644
Dawid Kostka <dpkostka@gmail.com>
2023-10-26 12:57:36 +0200

    urlaub nov3

--- components/Urlaub.jsx
+++ components/Urlaub.jsx
@@ -9,15 +9,12 @@ function Urlaub() {
         whileInView={{ y: [80, 0], opacity: [0, 1] }}
         transition={{ duration: 0.75 }}
       >
-        <h2>Wir sind im Urlaub!</h2>
+        {/* <h2>Wir sind im Urlaub!</h2> */}
         <p>
           {" "}
           Von <b>26.10 - 29.10.2023</b> und von <b>01.11 - 05.11.2023</b> ist
-          die Ordination geschlossen. Wir bitten um Verständnis, dass daher
-          keine Anfragen beantwortet werden können. In dringenden Notfällen
-          suchen Sie bitte eine Tierarztpraxis in Ihrer Nähe auf. Wir kümmern
-          uns um Ihr Anliegen nach dem Urlaub. <br /> <br /> Liebe Grüße Dr.
-          Bully und sein Team!
+          die Ordination geschlossen. Wir bitten um Verständnis.
+          <br /> <br /> Liebe Grüße Dr. Bully und sein Team!
         </p>
       </motion.div>
     </div>
