ad08fdb8a0652b9f1501bcbbfd56fab010b8d403
Dawid Kostka <dpkostka@gmail.com>
2024-06-24 23:47:44 +0200

    v1.3.5 added urlaub and teamFoto api

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,22 @@
 import Head from "next/head";
-
 import styles from "../styles/Home.module.css";
-
 import Hero from "../components/Hero";
 import InfoHero from "../components/InfoHero";
 import Block from "../components/Block";
 import FullTeam from "../components/FullTeam";
-
 import { getTeamFoto, getTeamMember, getUrlaub } from "../services";
 import { getBlogPost } from "../services/blogService";
-
 import { FloatingWhatsApp } from "react-floating-whatsapp";
 import PageFeed from "../components/PageFeed";
-
 import faceWA from "../public/images/face.jpg";
 import Urlaub from "../components/Urlaub";
-
 import CookieConsent from "react-cookie-consent";
 
 export default function Home({ data, urlaub }) {
   console.log(urlaub);
+
+  const isUrlaub =
+    urlaub.length > 0 && urlaub[0]?.node?.urlaubAnzeigen === true;
 
   return (
     <div className={styles.container}>
@@ -32,11 +29,11 @@ export default function Home({ data, urlaub }) {
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
 
-      {urlaub[0].node.urlaub === true ? <Urlaub urlaub={urlaub} /> : null}
-      {/* <Urlaub urlaub={urlaub} /> */}
+      {isUrlaub && <Urlaub urlaub={urlaub} />}
       <FullTeam data={data} />
       <Block />
       <PageFeed />
+      {/* Uncomment the following if WhatsApp support is needed */}
       {/* <FloatingWhatsApp
         phoneNumber="436602750881"
         accountName="Tierarztpraxis Quehenberger"
@@ -46,6 +43,7 @@ export default function Home({ data, urlaub }) {
         messageDelay={1.5}
         notificationDelay={5}
       /> */}
+      {/* Uncomment the following if cookie consent is needed */}
       {/* <CookieConsent
         location="bottom"
         buttonText="Akzeptieren"
@@ -66,8 +64,20 @@ export default function Home({ data, urlaub }) {
 }
 
 export async function getStaticProps() {
-  const data = (await getTeamFoto()) || [];
-  const urlaub = (await getUrlaub()) || [];
+  let data = [];
+  let urlaub = [];
+
+  try {
+    data = (await getTeamFoto()) || [];
+  } catch (error) {
+    console.error("Error fetching team photos:", error);
+  }
+
+  try {
+    urlaub = (await getUrlaub()) || [];
+  } catch (error) {
+    console.error("Error fetching vacation info:", error);
+  }
 
   return {
     props: {
