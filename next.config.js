5e57c46d7dcf342e64fe9b2a460e21656cf6435b
oblodev <dpkostka@gmail.com>
2025-08-19 13:30:32 +0200

    team image fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+// next.config.js
 module.exports = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["media.graphassets.com", "external-vie1-1.xx.fbcdn.net"],
+    domains: [
+      "media.graphassets.com",
+      "external-vie1-1.xx.fbcdn.net",
+      "eu-central-1-shared-euc1-02.graphassets.com"
+    ],
   },
   experimental: {
     workerThreads: false,
